652d33414d1cfa2e0ce8161ee0789b809812e545
ceciliaccwei <ceciliawei@yahoo.com>
2018-10-23 14:57:36 -0600

    signup screen

--- app/src/main/java/com/cmput301f18t25/healthx/Login.java
+++ app/src/main/java/com/cmput301f18t25/healthx/Login.java
@@ -1,7 +1,9 @@
 package com.cmput301f18t25.healthx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+    }
+
+    public void toSignUp(View view) {
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/cmput301f18t25/healthx/Signup.java
@@ -0,0 +1,31 @@
+package com.cmput301f18t25.healthx;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
