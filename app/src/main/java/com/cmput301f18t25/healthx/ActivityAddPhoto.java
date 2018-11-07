4bf6108f012248d9678be1b7702486c8bce44729
Ivan Lim <aznlim17@gmail.com>
2018-11-07 15:56:58 -0700

    Sending bitmap back to activity work

--- app/src/main/java/com/cmput301f18t25/healthx/ActivityAddPhoto.java
+++ app/src/main/java/com/cmput301f18t25/healthx/ActivityAddPhoto.java
@@ -5,28 +5,28 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
+
 public class ActivityAddPhoto extends AppCompatActivity {
     ImageView imageView;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_photo);
 
-        Button btnCamera = findViewById(R.id.btnCamera);
+        btnSave = findViewById(R.id.savePic);
         imageView = findViewById(R.id.imageView);
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
-            }
-        });
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+
     }
 
     @Override
@@ -34,5 +34,18 @@ public class ActivityAddPhoto extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap) data.getExtras().get("data");
         imageView.setImageBitmap(bitmap);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // CHANGE TO SEND TO RECORD WHEN ADD RECORD IS FINISHED
+                Intent pictureIntent = new Intent();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                pictureIntent.putExtra("image", byteArray);
+            }
+        });
     }
 }
