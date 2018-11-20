575f357d2ad5f07a223efcae21858995c3b76bc4
Aida <aradu@ualberta.ca>
2018-11-19 17:52:59 -0700

    tests fixed and mock

--- app/src/test/java/com/cmput301f18t25/healthx/UserTest.java
+++ app/src/test/java/com/cmput301f18t25/healthx/UserTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     public String test_name = "name";
-    public String test_id = "id";
+    public String test_username = "usrname";
     public String test_phone_number = "1234567890";
     public String test_email = "patient@email.com";
     public String test_status = "patient";
@@ -25,20 +25,20 @@ public class UserTest {
 
     @Test
     public void testCloneUser(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
 
         assertEquals(user.getName(),test_name);
-        assertEquals(user.getUserId(),test_id);
+        assertEquals(user.getUsername(),test_username);
         assertEquals(user.getPhoneNumber(),test_phone_number);
         assertEquals(user.getEmail(),test_email);
         assertEquals(user.getStatus(),test_status);
 
-        User user2 = new User("name2","id2","0987654321","doctor@email.com","care provider");
+        User user2 = new User("name2","usrname2","0987654321","doctor@email.com","care provider");
 
         user.cloneUser(user2);
 
         assertEquals(user.getName(),"name2");
-        assertEquals(user.getUserId(),"id2");
+        assertEquals(user.getUsername(),"usrname2");
         assertEquals(user.getPhoneNumber(),"0987654321");
         assertEquals(user.getEmail(),"doctor@email.com");
         assertEquals(user.getStatus(),"care provider");
@@ -47,43 +47,43 @@ public class UserTest {
 
     @Test
     public void testGetName(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         assertEquals(user.getName(),test_name);
     }
 
     @Test
     public void testSetName(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         String new_name = "new_name";
         user.setName(new_name);
         assertEquals(user.getName(),new_name);
     }
 
     @Test
-    public void testGetUserId(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
-        assertEquals(user.getUserId(),test_id);
+    public void testGetUsername(){
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
+        assertEquals(user.getUsername(),test_username);
 
     }
 
     @Test
-    public void testSetUserId(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
-        String new_id = "new_id";
-        user.setUserId(new_id);
-        assertEquals(user.getUserId(),new_id);
+    public void testSetUsername(){
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
+        String new_username = "new_usrnm";
+        user.setUsername(new_username);
+        assertEquals(user.getUsername(),new_username);
     }
 
     @Test
     public void testGetPhoneNumber(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         assertEquals(user.getPhoneNumber(),test_phone_number);
 
     }
 
     @Test
     public void testSetPhoneNumber(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         String new_number = "7564536123";
         user.setPhoneNumber(new_number);
         assertEquals(user.getPhoneNumber(),new_number);
@@ -91,14 +91,14 @@ public class UserTest {
 
     @Test
     public void testGetEmail(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         assertEquals(user.getEmail(),test_email);
 
     }
 
     @Test
     public void testSetEmail(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         String new_email = "new_email@email.com";
         user.setEmail(new_email);
         assertEquals(user.getEmail(),new_email);
@@ -106,14 +106,14 @@ public class UserTest {
 
     @Test
     public void testGetReminderFrequency(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         assertNull(user.getReminderFrequency());
 
     }
 
     @Test
     public void testSetReminderFrequency(){
-        User user = new User(test_name,test_id,test_phone_number,test_email,test_status);
+        User user = new User(test_name,test_username,test_phone_number,test_email,test_status);
         String new_freq = "daily";
         user.setReminderFrequency(new_freq);
         assertEquals(user.getReminderFrequency(),new_freq);
