6b73b51c646aa30b1ffcb065d3cee30ab5405016
Aida <aradu@ualberta.ca>
2018-12-02 00:13:22 -0700

    cant run yet because ES is evil

--- app/src/androidTest/java/com/cmput301f18t25/healthx/AddProblemTest.java
+++ app/src/androidTest/java/com/cmput301f18t25/healthx/AddProblemTest.java
@@ -5,22 +5,29 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import com.robotium.solo.Solo;
 
+import android.util.Log;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class AddProblemTest extends ActivityTestRule<Login> {
 
     public String test_username = "usrname";
-    public String test_title = "title";
+    // make a dif title each time we test it, so we're not mixing up problems
+    public String test_title = "title"+RandomStringUtils.randomAlphanumeric(3);
     public String test_description = "description of problem";
 
     private Solo solo;
@@ -82,6 +89,12 @@ public class AddProblemTest extends ActivityTestRule<Login> {
         solo.clickOnView(solo.getView(R.id.save_button));
         boolean next_view4 = solo.waitForActivity(ViewProblemList.class,3000);
         assertTrue("did not go to problem list",next_view4);
+
+        ArrayList<TextView> probs = solo.clickInRecyclerView(R.id.problemTitle, R.id.recycler_list);
+        Log.i("???", probs.toString());
+
+        assertTrue("problem not shown",solo.waitForText(test_title,1,3000,true));
+        assertTrue("problem not shown",solo.searchText(test_title,1,true));
 
     }
 }
