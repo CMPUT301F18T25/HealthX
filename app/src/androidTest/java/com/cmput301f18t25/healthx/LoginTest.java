dfbfcd5368eb7a0e6164fa7b1408ff858644833d
Aida <aradu@ualberta.ca>
2018-12-02 23:08:31 -0700

    just patient

--- app/src/androidTest/java/com/cmput301f18t25/healthx/LoginTest.java
+++ app/src/androidTest/java/com/cmput301f18t25/healthx/LoginTest.java
@@ -5,8 +5,10 @@ import android.support.test.rule.ActivityTestRule;
 import com.robotium.solo.Solo;
 
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,19 +17,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class LoginTest extends ActivityTestRule<Login>{
+public class LoginTest extends ActivityTestRule<Signup>{
 
-    public String test_username = "usrname";
+    public String test_name = "name"+RandomStringUtils.randomAlphabetic(3);
+
+    // make a dif username each time we test it, so we're not mixing up users
+    public String test_username = "usrname"+RandomStringUtils.randomAlphanumeric(3);
+    public String test_phone_number = "1234567890";
+    public String test_email = test_username+"@email.com";
     private Solo solo;
 
 
     public LoginTest() {
-        super(Login.class);
+        super(Signup.class);
     }
 
     @Rule
-    public ActivityTestRule<Login> activityTestRule =
-            new ActivityTestRule<>(Login.class);
+    public ActivityTestRule<Signup> activityTestRule =
+            new ActivityTestRule<>(Signup.class);
 
 
     @Before
@@ -44,15 +51,37 @@ public class LoginTest extends ActivityTestRule<Login>{
     @Test
     public void testLogin() throws Exception {
 
-        solo.assertCurrentActivity("wrong activity", Login.class);
+        solo.assertCurrentActivity("wrong activity",Login.class);
+        solo.clickOnView(solo.getView(R.id.link_signup));
+
+        solo.assertCurrentActivity("wrong activity", Signup.class);
 
 
-        EditText id = (EditText) solo.getView(R.id.loginUserID);
+        EditText id = (EditText) solo.getView(R.id.input_id);
+        EditText name = (EditText) solo.getView(R.id.input_name);
+        EditText email = (EditText) solo.getView(R.id.input_email);
+        EditText phone = (EditText) solo.getView(R.id.input_phone);
+
+        RadioButton patient_btn = (RadioButton) solo.getView(R.id.radio_patient);
+
+        solo.enterText(id,test_username);
+        solo.enterText(name,test_name);
+        solo.enterText(email,test_email);
+        solo.enterText(phone,test_phone_number);
+        solo.clickOnView(patient_btn);
+
+        solo.clickOnView(solo.getView(R.id.btn_signup));
+
+
+        assertTrue(solo.waitForActivity(Login.class));
+
+
+        EditText user_id = (EditText) solo.getView(R.id.loginUserID);
 
         solo.clickOnView(solo.getView(R.id.btn_login));
         assertTrue("toast not shown",solo.waitForText("Invalid Credentials!",1,3000));
 
-        solo.enterText(id,test_username);
+        solo.enterText(user_id,test_username);
         solo.clickOnView(solo.getView(R.id.btn_login));
 
         boolean next_view = solo.waitForActivity(ViewProblemList.class, 3000);
