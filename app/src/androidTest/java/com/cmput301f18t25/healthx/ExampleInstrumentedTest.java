23086764cfee0e67bc2698d866fa150fb302afe8
ceciliaccwei <ceciliawei@yahoo.com>
2018-12-03 04:32:39 -0700

    ganbatteeeeeee

--- app/src/androidTest/java/com/cmput301f18t25/healthx/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cmput301f18t25/healthx/ExampleInstrumentedTest.java
@@ -1,3 +1,12 @@
+/*
+ * Class Name: ExampleInstrumentedTest
+ *
+ * Version: Version 1.0
+ *
+ * Date : December 3, 2018
+ *
+ * Copyright (c) Team 25, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
 package com.cmput301f18t25.healthx;
 
 import android.content.Context;
--- app/src/test/java/com/cmput301f18t25/healthx/UserTest.java
+++ app/src/test/java/com/cmput301f18t25/healthx/UserTest.java
@@ -1,6 +1,11 @@
 /*
- *  * Copyright (c) Team 25, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ * Class Name: UserTest
  *
+ * Version: Version 1.0
+ *
+ * Date : December 3, 2018
+ *
+ * Copyright (c) Team 25, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
  */
 
 package com.cmput301f18t25.healthx;
