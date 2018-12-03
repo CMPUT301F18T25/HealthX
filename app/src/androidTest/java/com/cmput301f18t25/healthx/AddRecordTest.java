7fd6546ebd72e42eab4cc8d7889a107a8d29e2b1
Aida <aradu@ualberta.ca>
2018-12-02 23:08:06 -0700

    add record

--- app/src/androidTest/java/com/cmput301f18t25/healthx/AddRecordTest.java
+++ app/src/androidTest/java/com/cmput301f18t25/healthx/AddRecordTest.java
@@ -92,6 +92,7 @@ public class AddRecordTest extends ActivityTestRule<Login> {
         solo.enterText(phone,test_phone_number);
         solo.clickOnView(patient_btn);
         solo.clickOnView(solo.getView(R.id.btn_signup));
+        solo.wait(wait_time);
 
 
         assertTrue("did not go to login", solo.waitForActivity(Login.class));
@@ -130,6 +131,8 @@ public class AddRecordTest extends ActivityTestRule<Login> {
         assertTrue("problem title not shown",solo.waitForText(test_title,1,5000,true));
         solo.clickOnText(test_title,1,true);
         assertTrue(solo.waitForText("View "+test_title));
+        assertTrue("did not go to record list",solo.waitForActivity(ViewRecordList.class));
+
 
         solo.clickOnView(solo.getView(R.id.fab));
         assertTrue("did not go to add record",solo.waitForActivity(ActivityAddRecord.class));
@@ -137,6 +140,30 @@ public class AddRecordTest extends ActivityTestRule<Login> {
         EditText record_title_in = (EditText) solo.getView(R.id.record_title);
         DatePicker record_date_in = (DatePicker) solo.getView(R.id.dateStarted_input);
         EditText record_description_in = (EditText) solo.getView(R.id.description_input);
+
+        solo.enterText(record_title_in,test_title_record);
+        solo.setDatePicker(record_date_in,test_year,test_month,test_day);
+        solo.enterText(record_description_in,test_description_record);
+
+        // save record and go to record list
+
+        solo.clickOnView(solo.getView(R.id.save_button));
+        assertTrue("did not go to record list",solo.waitForActivity(ViewRecordList.class));
+
+        assertTrue("record title not shown",solo.waitForText(test_title_record,1,5000,true));
+        assertTrue("rec desc not shown",solo.waitForText(test_description_record,1,5000,true));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.set(test_year, test_month, test_day);
+        Date to_show = cal.getTime();
+        String display_date = simpleDateFormat.format(to_show);
+        Log.i("date",display_date + "    " + test_year +" "+ test_month+" " + test_day);
+
+
+        assertTrue("rec date not shown",solo.waitForText(Pattern.quote(display_date),1,5000,true));
+
+
 
 
 
